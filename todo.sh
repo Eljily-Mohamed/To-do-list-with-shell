5d1a2b5cc403e6a2752e483782671de9629a3442
hamade-kj <hamadekj15@gmail.com>
2023-01-11 10:51:07 +0100

    end banner

--- todo.sh
+++ todo.sh
@@ -1,14 +1,64 @@
 #!/bin/bash
-#affiche bannner fonction 
-function banner (){
-     echo "$USER welcome in the to do list"
-     
+#--------------------------------------------------------------------+
+#Color picker, usage: printf ${BLD}${CUR}${RED}${BBLU}"Hello!)"${DEF}|
+#-------------------------+--------------------------------+---------+
+#       Text color        |       Background color         |         |
+#-----------+-------------+--------------+-----------------+         |
+# Base color|Lighter shade|  Base color  | Lighter shade   |         |
+#-----------+-------------+--------------+-----------------+         |
+BLK='\e[30m'; blk='\e[90m'; BBLK='\e[40m'; bblk='\e[100m' #| Black   |
+RED='\e[31m'; red='\e[91m'; BRED='\e[41m'; bred='\e[101m' #| Red     |
+GRN='\e[32m'; grn='\e[92m'; BGRN='\e[42m'; bgrn='\e[102m' #| Green   |
+YLW='\e[33m'; ylw='\e[93m'; BYLW='\e[43m'; bylw='\e[103m' #| Yellow  |
+BLU='\e[34m'; blu='\e[94m'; BBLU='\e[44m'; bblu='\e[104m' #| Blue    |
+MGN='\e[35m'; mgn='\e[95m'; BMGN='\e[45m'; bmgn='\e[105m' #| Magenta |
+CYN='\e[36m'; cyn='\e[96m'; BCYN='\e[46m'; bcyn='\e[106m' #| Cyan    |
+WHT='\e[37m'; wht='\e[97m'; BWHT='\e[47m'; bwht='\e[107m' #| White   |
+#----------------------------------------------------------+---------+
+# Effects                                                            |
+#--------------------------------------------------------------------+
+DEF='\e[0m'   #Default color and effects                             |
+BLD='\e[1m'   #Bold\brighter                                         |
+DIM='\e[2m'   #Dim\darker                                            |
+CUR='\e[3m'   #Italic font                                           |
+UND='\e[4m'   #Underline                                             |
+INV='\e[7m'   #Inverted                                              |
+COF='\e[?25l' #Cursor Off                                            |
+CON='\e[?25h' #Cursor On                                             |
+#--------------------------------------------------------------------+
+# Text positioning, usage: XY 10 10 "Hello World!"                   |
+XY   () { printf "\e[${2};${1}H${3}";   } #                          |
+#--------------------------------------------------------------------+
+# Print line, usage: line - 10 | line -= 20 | line "Hello World!" 20 |
+line () { printf -v LINE "%$2s"; printf -- "${LINE// /$1}"; } #      |
+# Create sequence like {0..X}                                        |
+cnt () { printf -v _N %$1s; _N=(${_N// / 1}); printf "${!_N[*]}"; } #|
+#--------------------------------------------------------------------+
+
+welcome=(''
+    $RED" ____      ____  ________  _____       ______    ___   ____    ____  ________      \n"$DEF
+    $RED"|_  _|    |_  _||_   __  ||_   _|    .' ___  | .'   \`.|_   \  /   _||_   __  |    \n"$DEF
+    $GRN"  \ \  /\  / /    | |_ \_|  | |     / .'   \_|/  .-.  \ |   \/   |    | |_ \_|     \n"$DEF
+    $GRN"   \ \/  \/ /     |  _| _   | |   _ | |       | |   | | | |\  /| |    |  _| _      \n"$DEF
+    $BLU"    \  /\  /     _| |__/ | _| |__/ |\ \`.___.'\\\\\  \`-'  /_| |_\/_| |_  _| |__/ |\n"$DEF
+    $BLU"     \/  \/     |________||________| \`.____ .' \`.___.'|_____||_____||________|   \n"$DEF
+    
+)
+
+banner (){
+     for i in {1..5}
+     do 
+       echo ''
+     done
+
+     echo "$USER welcome in your to do list " 
 }
 
 
 #fonction main point d'excution de programme
-function main (){
-    banner 
-}
+ function main (){
+     printf "${welcome[*]}"
+     banner 
+ }
 
-main
+# main
